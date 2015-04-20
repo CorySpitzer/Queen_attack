fc715836c5986771249c678430fcb8f2394ed5df
Guest User <student@epicodus.com>
2015-04-20 16:57:44 -0700

    Test that vertical coordinates are legal, pass test

--- lib/queen_attack.rb
+++ lib/queen_attack.rb
@@ -1,6 +1,9 @@
 class Array
   define_method(:queen_attack?) do |coordinates|
+    #self and coordinates are 2 element arrays.
     if self[0] == coordinates[0]
+      true
+    elsif self[1] == coordinates[1]
       true
     else
       false
